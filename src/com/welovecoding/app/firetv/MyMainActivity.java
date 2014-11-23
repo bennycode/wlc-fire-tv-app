912f4e668afdcd23e649df9fac351c6bba5f2443
Benny Neugebauer <bn@bennyn.de>
2014-11-23 21:36:53 +0100

    Added a List view.

--- src/com/welovecoding/app/firetv/MyMainActivity.java
+++ src/com/welovecoding/app/firetv/MyMainActivity.java
@@ -2,14 +2,28 @@ package com.welovecoding.app.firetv;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MyMainActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        // setContentView(R.layout.main);
+
+        setContentView(R.layout.activity_list_view_android_example);
+        ListView listView = (ListView) findViewById(R.id.listView2);
+
+        String[] values = new String[]{
+                "Android List View",
+                "Adapter implementation",
+                "Simple List View In Android",
+                "Create List View Android",
+                "Android Example",
+                "List View Source Code",
+                "List View Array Adapter",
+                "Android Example List View"
+        };
+
     }
 }
